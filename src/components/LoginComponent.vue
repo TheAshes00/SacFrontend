2d6fe0b6e0915217ec181a4a037da708f1f258bd
TheAshes00 <121140974+TheAshes00@users.noreply.github.com>
2024-10-31 01:07:33 -0600

    Remove unused created function

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -22,9 +22,6 @@ export default {
             }
         }
     },
-    async created(){
-      this.studentStore.getAllAcademicEntities();
-    },
     components: {
         PopUpWarning,
         LoaderComponent
