498b502c58a2fda12269633b6fbedc509922349e
TheAshes00 <121140974+TheAshes00@users.noreply.github.com>
2024-10-31 00:46:28 -0600

    Post tested local

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -64,7 +64,7 @@ export default {
                 }
                 else
                 {
-                    this.subActivateAdminLogin()
+                    await this.subActivateAdminLogin()
                 }
 
             }
